d143b553bd72298b67f9868d2175e81139c4f705
brian kimani <briankimani013@gmail.com>
2018-08-30 17:15:00 +0300

    all changes made

---
+++ js/script3.js
@@ -0,0 +1,247 @@
+$(document).ready(function() {
+  $("#quest1-0").click(function() {
+    $("#question1-0").hide();
+    $("#question2-0").show();
+  });
+  $("#quest1-1").click(function() {
+    $("#question1-0").hide();
+    $("#question2-1").show();
+  });
+  $("#quest1-2").click(function() {
+    $("#question1-0").hide();
+    $("#question2-2").show();
+  });
+  $("#quest1-3").click(function() {
+    $("#question1-0").hide();
+    $("#question2-3").show();
+  });
+
+  // Level 2
+  $("#quest2-0").click(function() {
+    $("#question2-0").hide();
+    $("#question3-0").show();
+  });
+  $("#quest2-1").click(function() {
+    $("#question2-0").hide();
+    $("#question3-1").show();
+  });
+  $("#quest2-2").click(function() {
+    $("#question2-1").hide();
+    $("#question3-2").show();
+  });
+  $("#quest2-3").click(function() {
+    $("#question2-1").hide();
+    $("#question3-3").show();
+  });
+  $("#quest2-4").click(function() {
+    $("#question2-2").hide();
+    $("#question3-4").show();
+  });
+  $("#quest2-5").click(function() {
+    $("#question2-2").hide();
+    $("#question3-5").show();
+  });
+  $("#quest2-6").click(function() {
+    $("#question2-3").hide();
+    $("#question3-6").show();
+  });
+  $("#quest2-7").click(function() {
+    $("#question2-3").hide();
+    $("#question3-7").show();
+  });
+
+  // Level 3
+  $("#quest3-0").click(function() {
+    $("#question3-0").hide();
+    $("#question4-0").show();
+  });
+  $("#quest3-1").click(function() {
+    $("#question3-0").hide();
+    $("#question4-1").show();
+  });
+  $("#quest3-2").click(function() {
+    $("#question3-1").hide();
+    $("#question4-2").show();
+  });
+  $("#quest3-3").click(function() {
+    $("#question3-1").hide();
+    $("#question4-3").show();
+  });
+  $("#quest3-4").click(function() {
+    $("#question3-2").hide();
+    $("#question4-4").show();
+  });
+  $("#quest3-5").click(function() {
+    $("#question3-2").hide();
+    $("#question4-5").show();
+  });
+  $("#quest3-6").click(function() {
+    $("#question3-3").hide();
+    $("#question4-6").how();
+  });
+  $("#quest3-7").click(function() {
+    $("#question3-3").hide();
+    $("#question4-7").show();
+  });
+  $("#quest3-8").click(function() {
+    $("#question3-4").hide();
+    $("#question4-8").show();
+  });
+  $("#quest3-9").click(function() {
+    $("#question3-4").hide();
+    $("#question4-9").show();
+  });
+  $("#quest3-10").click(function() {
+    $("#question3-5").hide();
+    $("#question4-10").show();
+  });
+  $("#quest3-11").click(function() {
+    $("#question3-5").hide();
+    $("#question4-11").show();
+  });
+  $("#quest3-12").click(function() {
+    $("#question3-6").hide();
+    $("#question4-12").show();
+  });
+  $("#quest3-13").click(function() {
+    $("#question3-6").hide();
+    $("#question4-13").show();
+  });
+  $("#quest3-14").click(function() {
+    $("#question3-7").hide();
+    $("#question4-14").how();
+  });
+  $("#quest3-15").click(function() {
+    $("#question3-7").hide();
+    $("#question4-15").show();
+  });
+  // Last Level
+  $("#quest4-0").click(function() {
+    $("#question4-0").hide();
+    $("#question2-2").show();
+  });
+  $("#quest4-1").click(function() {
+    $("#question4-0").hide();
+    $("#question2-3").show();
+  });
+  $("#quest4-2").click(function() {
+    $("#question4-1").hide();
+    $("#question2-1").show();
+  });
+  $("#quest4-3").click(function() {
+    $("#question4-1").hide();
+    $("#questoin3-0").show();
+  });
+  $("#quest4-4").click(function() {
+    $("#question4-2").hide();
+    $("#question3-6").show();
+  });
+  $("#quest4-5").click(function() {
+    $("#question4-2").hide();
+    $("#question3-7").show();
+  });
+  $("#quest4-6").click(function() {
+    $("#question4-3").hide();
+    $("#win").show();
+  });
+  $("#quest4-7").click(function() {
+    $("#question4-3").hide();
+    $("#question2-1").show();
+  });
+  $("#quest4-8").click(function() {
+    $("#question4-4").hide();
+    $("#question3-0").show();
+  });
+  $("#quest4-9").click(function() {
+    $("#question4-4").hide();
+    $("#question2-0").show();
+  });
+  $("#quest4-10").click(function() {
+    $("#question4-5").hide();
+    $("#question3-4").show();
+  });
+  $("#quest4-11").click(function() {
+    $("#question4-5").hide();
+    $("#question3-7").show();
+  });
+  $("#quest4-12").click(function() {
+    $("#question4-6").hide();
+    $("#question3-4").show();
+  });
+  $("#quest4-13").click(function() {
+    $("#question4-6").hide();
+    $("#question2-0").show();
+  });
+  $("#quest4-14").click(function() {
+    $("#question4-7").hide();
+    $("#question3-4").show();
+  });
+  $("#quest4-15").click(function() {
+    $("#question4-7").hide();
+    $("#question3-2").show();
+  });
+  $("#quest4-16").click(function() {
+    $("#question4-8").hide();
+    $("#question3-5").show();
+  });
+  $("#quest4-17").click(function() {
+    $("#question4-8").hide();
+    $("#question3-7").show();
+  });
+  $("#quest4-18").click(function() {
+    $("#question4-9").hide();
+    $("#question2-2").show();
+  });
+  $("#quest4-19").click(function() {
+    $("#question4-9").hide();
+    $("#question3-2").show();
+  });
+  $("#quest4-20").click(function() {
+    $("#question4-10").hide();
+    $("#question2-2").show();
+  });
+  $("#quest4-21").click(function() {
+    $("#question4-10").hide();
+    $("#question2-3").show();
+  });
+  $("#quest4-22").click(function() {
+    $("#question4-11").hide();
+    $("#question2-1").show();
+  });
+  $("#quest4-23").click(function() {
+    $("#question4-11").hide();
+    $("#question3-2").show();
+  });
+  $("#quest4-24").click(function() {
+    $("#question4-12").hide();
+    $("#question2-0").show();
+  });
+  $("#quest4-25").click(function() {
+    $("#question4-12").hide();
+    $("#question3-3").show();
+  });
+  $("#quest4-26").click(function() {
+    $("#question4-13").hide();
+    $("#question3-7").show();
+  });
+  $("#quest4-27").click(function() {
+    $("#question4-13").hide();
+    $("#question3-6").show();
+  });
+  $("#quest4-28").click(function() {
+    $("#question4-14").hide();
+    $("#question3-4").show();
+  });
+  $("#quest4-29").click(function() {
+    $("#question4-14").hide();
+    $("#question3-3").show();
+  });
+  $("#quest4-30").click(function() {
+    $("#question4-15").hide();
+    $("#question3-0").show();
+  });
+  $("#quest4-31").click(function() {
+    $("#question4-15").hide();
+    $("#question1-0").show();
+  });
+});
